49370bfec1972209c04dec4dd58880fdecfb1e3c
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2024-01-09 15:09:01 +0100

    fix numbers with correct seed

--- tests/testthat/test-one_minus_kaplan_meier.R
+++ tests/testthat/test-one_minus_kaplan_meier.R
@@ -8,7 +8,7 @@ test_that("one_minus_kaplan_meier works as expected", {
     haz_soft = 0.5
   )
   result <- one_minus_kaplan_meier(data = df, tau = 4)
-  expected <- c(ae_prob = 0.54147805, ae_prob_var = 0.02275271)
+  expected <- c(ae_prob = 0.3771350, ae_prob_var = 0.0260535)
   expect_equal(result, expected, tolerance = 1e-4)
 })
 
